ce2ec1e0ff9e8b10c4b2e0ed00d16af7dbfc239f
saasbook <saasbook@saasbook.(none)>
2014-06-01 16:35:49 -0300

    hw 2 part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,15 +7,33 @@ class MoviesController < ApplicationController
   end
 
   def index
-    #@movies = Movie.all
-    @movies = Movie.scoped
-    if params['sort'] == 'title' then 
-      @movies = @movies.order('title')
-      @title = 'hilite'
+	# create an array of all ratings using a class method defined in movie.rb
+    @all_ratings = Movie.rating_list
+    
+    # Checkbox code - used in view file: index.html.haml
+    #  When ratings params are passed (some boxes are unchecked)      
+    if params[:ratings] 
+      # create a hash with all keys being false  
+      @check = Hash[Movie.rating_list.collect {|r| [r,false]}]
+      # then merge with the passed params, changing the checked ones ("1") to true
+      # @check = @check.merge!(params[:ratings].each_key {|k|  params[:ratings][k] = true})
+      # note: able to shorten the above by changing the views check_box_tag default from 1 to true
+      @check = @check.merge!(params[:ratings])
+    else
+      # check all boxes by default if no params are passed
+      @check = Hash[Movie.rating_list.collect {|r| [r,true]}]
     end
-    else if params['sort'] == 'release_date' then
-    @movies = @movies.order('release_date') 
-    @release_date = 'hilite'
+
+    # gets all the movies from the database
+    @movies = Movie.all
+
+    # restrict the query based on the rating boxes the user has checked
+    @movies = Movie.where(:rating => params[:ratings].keys) if params[:ratings]
+
+    # handle the events when the title and release dates need sorted
+    if params['sort']
+      @movies = Movie.order(params['sort'])
+      instance_variable_set("@" + params['sort'], 'hilite')
     end
   end
 
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ActiveRecord::Base
   attr_accessible :title, :rating, :description, :release_date
+
+  def self.rating_list
+	return Movie.select(:rating).map(&:rating).uniq.sort
+  end
 end
