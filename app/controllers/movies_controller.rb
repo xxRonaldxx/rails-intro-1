a2739be10aea0f1e1d8987c937f00c83a12800b9
saasbook <saasbook@saasbook.(none)>
2014-06-01 21:49:39 -0300

    refactored index

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -22,30 +22,16 @@ class MoviesController < ApplicationController
     @all_ratings = Movie.rating_list
     
     # Checkbox code - used in view file: index.html.haml
-    #  When ratings params are passed (some boxes are unchecked)      
-    if params[:ratings] 
-      # create a hash with all keys being false  
-      @check = Hash[Movie.rating_list.collect {|r| [r,false]}]
-      # then merge with the passed params, changing the checked ones ("1") to true
-      #@check = @check.merge!(params[:ratings].each_key {|k|  params[:ratings][k] = true})
-      # note: able to shorten the above by changing the views check_box_tag default from 1 to true
-      @check = @check.merge!(params[:ratings])
-    else
-      # check all boxes by default if no params are passed
-      @check = Hash[Movie.rating_list.collect {|r| [r,true]}]
-    end
-
-    # gets all the movies from the database
-    @movies = Movie.scoped
+    #  When ratings params are passed (some boxes are unchecked)         
+    @check = Hash[Movie.rating_list.collect {|r| [r,true]}]
+    @check = (params[:ratings]) if params[:ratings]
 
     # restrict the query based on the rating boxes the user has checked
-    @movies = @movies.where(:rating => params[:ratings].keys) if params[:ratings]
-
-    # handle the events when the title and release dates need sorted
-    if params['sort']
-      @movies = @movies.order(params['sort'])
-      instance_variable_set("@" + params['sort'], 'hilite')
-    end
+    @movies = Movie.where(:rating => @check.select {|k,v| v}.keys).order(params['sort'])
+    
+    # handle the events when the title and release dates need sorted   
+    instance_variable_set("@" + params['sort'], 'hilite') if params['sort']
+   
   end
 
   def new
